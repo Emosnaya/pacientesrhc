58f1e9a79440d0f4fc6859abea2d1fff28b8ccd4
emmanuel114 <emmanuel@dd3tech.mx>
2026-02-05 23:27:45 -0600

    fix: Agregar dominios de producción a CORS

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,9 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://pacientesrhc.com',
+        'https://www.pacientesrhc.com',
+        'https://api.pacientesrhc.com',
     ],
 
     'allowed_origins_patterns' => ['/localhost.*/'],
