0bf3d797b50e61a4c0b61d55726a8197aee40ea7
emmanuel114 <emmanuel@dd3tech.mx>
2026-04-01 23:20:16 -0600

    feat: change allowed origins cors

--- config/cors.php
+++ config/cors.php
@@ -20,10 +20,6 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
         'https://pacientesrhc.com/',
         'https://pacientesrhc.com',
         'https://www.pacientesrhc.com',
